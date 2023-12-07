c92f2bfb9e001c68bbc97917df65a5b9d3474b4f
narramuni <nmspavan@gmail.com>
2023-12-07 20:12:40 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PizzaJenkinsProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
